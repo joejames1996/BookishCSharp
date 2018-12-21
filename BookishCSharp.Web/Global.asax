aaf92b80bbf56ae66dee9f72bef90e872c86c6ca
joejames1996 <Joe.James@softwire.com>
2018-12-21 17:06:03 +0000

    Web initial commit

---
+++ BookishCSharp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookishCSharp.Web.MvcApplication" Language="C#" %>
---
+++ BookishCSharp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookishCSharp.Web.Startup))]
+namespace BookishCSharp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
